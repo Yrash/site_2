14a0c727a3293c82563490a4d2c282423f16d8b5
yrash <yrash1990@gmail.com>
2018-03-11 23:09:02 +0200

    paralax

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	pug = require('gulp-pug'),
+  ts = require('gulp-typescript'),
 	browserSync = require('browser-sync').create();
  
-gulp.task('default', [ 'sass','views', 'serve']);
+gulp.task('default', [ 'sass','views','ts', 'serve']);
 
 
 gulp.task('sass', function () {
@@ -12,7 +13,14 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(''))
     .pipe(browserSync.stream());
 });
- 
+gulp.task('ts', function () {
+    return gulp.src('src/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: 'main.js'
+        }))
+        .pipe(gulp.dest('src/'));
+}); 
 gulp.task('views', function buildHTML() {
   return gulp.src('./src/index.pug')
   .pipe(pug({pretty: true}))
@@ -28,7 +36,10 @@ gulp.task('serve', ['sass'], function() {
 
     gulp.watch('./src/style.sass', ['sass']).on('change', browserSync.reload);
     gulp.watch('./src/index.pug', ['views']).on('change', browserSync.reload);
-  	gulp.watch('./src/main.js' ).on('change', browserSync.reload);
+    gulp.watch('./src/main.js' ).on('change', browserSync.reload);
+  	gulp.watch('src/**/*.ts' ,['ts']).on('change', browserSync.reload);
     
 });
 
+
+ 
